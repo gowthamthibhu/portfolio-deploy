e96f823e9a3bdac5ec88a37854da95d0e9cd9616
gowthamthibhu <146013009+gowthamthibhu@users.noreply.github.com>
2025-01-14 14:55:45 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -3,15 +3,19 @@
  */
 const nextConfig = {
     output: 'export',
-   
+    
+    // Specify the base path for GitHub Pages deployment
+    basePath: '/portfolio-deploy',  // Replace 'your-repo-name' with your actual GitHub repo name
+    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
+    trailingSlash: true,
+  
     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     // skipTrailingSlashRedirect: true,
-   
+  
     // Optional: Change the output directory `out` -> `dist`
     // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+  };
+  
+  module.exports = nextConfig;
+  
