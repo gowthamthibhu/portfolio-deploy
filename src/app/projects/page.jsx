964d7b432ab206a2a70360e84803db5b966b989c
gowthamthibhu <146013009+gowthamthibhu@users.noreply.github.com>
2025-01-14 19:24:25 +0530

    projects

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -207,7 +207,7 @@ export default function Projects() {
             return (
               <div className={styles.modal} style={{backgroundColor: color}} key={`modal_${index}`}>
                 <Image 
-                  src={`/images/${src}`}
+                  src={`./images/${src}`}
                   width={300}
                   height={0}
                   alt="image"
